e40e6c1f6d19383bea70242d1c2bafcacdea75fa
Demis Getachew <demisgech@gmail.com>
2025-09-03 14:43:10 +0300

    Refactor: towards clean design

--- oops/todo-list.py
+++ oops/todo-list.py
@@ -13,52 +13,92 @@ class TodoList:
     def remove_task(self,id:int):
         self.__tasks = list(filter(lambda task:task['id'] != id, self.__tasks))
     
-    def mark_as_completed(self,id:int):
-        completed_task = [{**task,"completed":True} for task in self.__tasks if task['id'] == id]
-        self.remove_task(id)
-        self.__tasks = [*self.__tasks,*completed_task]
-
-    def display_tasks(self):
-        for task in self.__tasks:
-            print(f"{{id: { task['id']},title: {task['title']},completed: {task['completed']}}}")
+    def mark_as_completed(self, id: int):
+        self.__tasks = [
+            {**task, "completed": True} if task["id"] == id else task
+            for task in self.__tasks
+        ]
+        # or
+        # self.__tasks = list(
+        #     map(
+        #         lambda task: {**task, "completed": True} if task["id"] == id else task,
+        #         self.__tasks
+        #     ))
+    def get_all_task(self):
+        return self.__tasks
     
-    def display_uncompleted_tasks(self):
-        for task in self.__tasks:
-            if not task['completed']:
-                print(f"{{id: { task['id']},title: {task['title']},completed: {task['completed']}}}")
+    def get_uncompleted_tasks(self):
+        # return list(filter(lambda task: not task['completed'], self.__tasks))
+        return [task for task in self.__tasks if not task['completed']]
     
-    def display_completed_tasks(self):
-        for task in self.__tasks:
-            if task['completed']:
-                print(f"{{id: { task['id']},title: {task['title']},completed: {task['completed']}}}")
+    def get_completed_task(self):
+        # return list(filter(lambda task: task['completed'], self.__tasks))
+        return [task for task in self.__tasks if task['completed']]
     
     def search_task(self,title:str):
-        task = list(filter(lambda task:task['title'].lower() == title.lower(), self.__tasks))
-        return task
+        # return list(filter(lambda task: task['title'].lower() == title.lower(), self.__tasks))
+        return [task for task in self.__tasks if task['title'].lower() == title.lower()]
     
+    def sort_tasks(self):
+        self.__tasks = sorted(self.__tasks,key=lambda task:task['title'].lower())
+
+
+class TodoCLI:
+    def __init__(self,todo_list:TodoList,) -> None:
+        self.__todo_list = todo_list
+    
+    def display_task_list(self,task_list:list[dict],empty_message="No tasks found"):
+        if not task_list:
+            print(empty_message)
+        for task in task_list:
+            print(f"{{id: { task['id']},title: {task['title']},completed: {task['completed']}}}")   
+        
     def display_menu(self):
-        ...
-
-todo_list = TodoList()
-todo_list.add_task("new task")
-todo_list.add_task("another task")
-
-todo_list.display_tasks()
-
-# todo_list.remove_task(2)
-
-todo_list.mark_as_completed(1)
-print("After completed")
-todo_list.display_tasks()
-
-print("completed")
-todo_list.display_completed_tasks()
-
-print("Uncompleted Task")
-todo_list.display_uncompleted_tasks()
-
-# print("Searching...")
-# task = todo_list.search_task("Another task")
-
-
-# print(task)
+        print("""***** WELCOME TO TODO LIST SYSTEM ****
+1. Add Task
+2. Remove Task
+3. Mark Task as Completed
+4. Display All Tasks
+5. Display Uncompleted Tasks
+6. Display Completed Tasks
+7. Search Task
+8. Sort Tasks
+9. Exit
+""")
+    def run(self):
+        self.display_menu()
+        while True:
+            option = input("Option: ")
+            match option:
+                case '1':
+                    title = input("Title: ")
+                    self.__todo_list.add_task(title)
+                case '2':
+                    id = int(input("Id: "))
+                    self.__todo_list.remove_task(id)
+                case '3':
+                    id = int(input("Id: "))
+                    self.__todo_list.mark_as_completed(id)
+                case '4':
+                    self.display_task_list(self.__todo_list.get_all_task())
+                case '5':
+                    self.display_task_list(self.__todo_list.get_uncompleted_tasks())
+                case '6':
+                    self.display_task_list(self.__todo_list.get_completed_task())
+                case '7':
+                    title = input("Title: ")
+                    self.display_task_list(self.__todo_list.search_task(title))
+                case '8':
+                    self.__todo_list.sort_tasks()
+                case '9':
+                    print("Exiting...")
+                    break
+                case _:
+                    print("Invalid Option")
+                    
+                    
+                    
+if __name__ == "__main__":                               
+    todo_list = TodoList()
+    cli = TodoCLI(todo_list)
+    cli.run()
